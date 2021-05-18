06fe14a4bf4d922bac21c71984dc8971afc359ae
delta <h0568@naver.com>
2021-05-19 01:38:01 +0900

    [링크 추가, css 추가]

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+
+import Header from "./components/Header.jsx";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import About from "./pages/About.jsx";
+import Cities from "./pages/Cities.jsx";
+import Home from "./pages/Home.jsx";
+
+function App() {
+  const city = "Seoul";
+  return (
+    <Router>
+       <div className="App">
+          <Header region="KR" cityName={city}/>
+          <ul className="nevigation">
+            <li>
+              <Link to ="/">HOME</Link>
+            </li>
+            <li>
+              <Link to ="/about">About</Link>
+            </li>
+            <li>
+              <Link to ="/cities">Cities</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/cities">
+              <Cities />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+       </div>
+    </Router>
+  );
+}
+
+export default App;
